4525d19f1d39f2030997d40b2ddf52fc4d308136
lsprdev <github@lspr.dev>
2024-09-21 19:31:05 -0300

    Adicionando Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Arena Já API')
+    .setDescription('API para agendamentos de quadras.')
+    .setVersion('1.0')
+    .addTag('Tennis')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
