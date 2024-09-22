5117f29b897ec6e49b9415f10479cc5bc365e7ec
lsprdev <github@lspr.dev>
2024-09-21 23:57:20 -0300

    Configurando auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private authService: AuthService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  async index() {
+    return {
+      message: 'API desenvolvida por @lsprdev',
+      data: {
+        version: '1.0.0',
+      },
+    };
   }
+
+  // create a new user
+  @Post('api/signup')
+  async signup(@Request() req) {
+    return this.authService.signup(req.body);
+  }
+
+  // login with an existing user
+  @UseGuards(LocalAuthGuard)
+  @Post('api/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('api/users')
+  async users(@Request() req) {
+    return this.authService.getUsers();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('api/user/:id')
+  async user(@Request() req) {
+    return this.authService.getUser(req.params.id);
+  }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { ArenaModule } from './arena/arena.module';
 import { CourtModule } from './court/court.module';
 import { ScheduleModule } from './schedule/schedule.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UserModule, ArenaModule, CourtModule, ScheduleModule, PrismaModule],
+  imports: [UsersModule, ArenaModule, CourtModule, ScheduleModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
